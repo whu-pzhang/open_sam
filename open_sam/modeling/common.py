1c85a169b53c5c069b95b88ec4b4f761907d383b
whu-pzhang <pzhang.omega@gmail.com>
2023-12-20 13:48:29 +0800

    update

--- open_sam/modeling/common.py
+++ open_sam/modeling/common.py
@@ -3,9 +3,6 @@ from typing import Type
 import torch
 import torch.nn as nn
 
-from mmseg.models.utils import resize
-from mmpretrain.models.utils import to_2tuple
-
 
 class MLPBlock(nn.Module):
 
@@ -39,39 +36,4 @@ class LayerNorm2d(nn.Module):
         s = (x - u).pow(2).mean(1, keepdim=True)
         x = (x - u) / torch.sqrt(s + self.eps)
         x = self.weight[:, None, None] * x + self.bias[:, None, None]
-        return x
-
-
-class UpSampleLayer(nn.Module):
-
-    def __init__(
-        self,
-        mode="bicubic",
-        size=None,
-        factor=2,
-        align_corners=False,
-    ):
-        super(UpSampleLayer, self).__init__()
-        self.mode = mode
-        self.size = to_2tuple(size, 2) if size is not None else None
-        self.factor = None if self.size is not None else factor
-        self.align_corners = align_corners
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return resize(x, self.size, self.factor, self.mode, self.align_corners)
-
-
-class OpSequential(nn.Module):
-
-    def __init__(self, op_list):
-        super().__init__()
-        valid_op_list = []
-        for op in op_list:
-            if op is not None:
-                valid_op_list.append(op)
-        self.op_list = nn.ModuleList(valid_op_list)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        for op in self.op_list:
-            x = op(x)
         return x
